fba048cb40a4163a2fc65c70f1e7631035121438
Max Brosnahan <maximilianbrosnahan@gmail.com>
2015-12-07 04:04:37 +0400

    Convert main script into lib and add bin script

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 'use strict'
-let version = process.argv[2]
-let deviceName = process.argv[3]
-let url = process.argv[4]
 
-const async = require('async')
-const ios = require('./lib/ios')
-
-async.auto({
-  device: function(cb){
-    ios.getDevice(version, deviceName, cb)
-  },
-  startDevice: ['device', function(cb, results) {
-    if (results.device.state === 'Shutdown') {
-      ios.startDevice(results.device.id, cb)
-    } else {
-      cb(null)
-    }
-  }],
-  openURL: ['startDevice', function openURL(cb, results){
-    ios.openURL(results.device.id, url, cb)
-  }]
-}, function completed(err) {
-  if (err) {
-    console.log(err)
-    process.exit(1)
-  } else {
-    // Sleep until testem kills us
-    while (true);;
-  }
-})
+module.exports = {
+  ios: require('./lib/ios')
+}
